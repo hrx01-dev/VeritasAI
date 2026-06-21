c80f2d11a89416405cb17e32d1618c1086a06c37
Jayam Srivastava <jayamsrivastava99@gmail.com>
2026-06-21 17:45:22 +0530

    Removed secret keys exposed

--- src/app/lib/firebaseConfig.ts
+++ src/app/lib/firebaseConfig.ts
@@ -2,12 +2,12 @@
 // This config is used by the Firebase initialization module
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBRdVGqovt2oZrxfqhLdefsy_v3r1LVXTE",
-  authDomain: "veritasai-6e4ac.firebaseapp.com",
-  databaseURL: "https://veritasai-6e4ac-default-rtdb.firebaseio.com",
-  projectId: "veritasai-6e4ac",
-  storageBucket: "veritasai-6e4ac.appspot.com",
-  messagingSenderId: "634027149151",
-  appId: "1:634027149151:web:620c795ee8552c189698ea",
-  measurementId: "G-0KRJDDP1J4",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
